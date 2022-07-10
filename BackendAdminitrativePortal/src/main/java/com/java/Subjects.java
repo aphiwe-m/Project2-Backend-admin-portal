4ad1117e44ba8b13fd58fb846e5c320cd2204725
user <aphiwemambongwe@gmail.com>
2022-07-10 23:14:41 +0200

    first commit

---
+++ BackendAdminitrativePortal/src/main/java/com/java/Subjects.java
@@ -0,0 +1,31 @@
+package com.java;
+
+public class Subjects {
+
+	private int id;
+	private String name;
+
+	public Subjects(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+}
---
+++ BackendAdminitrativePortal/src/main/java/com/java/Teachers.java
@@ -0,0 +1,36 @@
+package com.java;
+
+public class Teachers {
+
+
+	private int id;
+	private String name;
+
+	public Teachers(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getname() {
+		return name;
+	}
+
+	public void setname(String name) {
+		this.name = name;
+	}
+
+
+	
+	}
+
+
